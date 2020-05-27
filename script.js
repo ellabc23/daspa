e3426b3a4e984470683bf38cb3ecce6feaf8f93d
Ella <33379153+ellabc23@users.noreply.github.com>
2020-05-26 20:39:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function login() {
 var username = document.getElementById('username').value;
 var password =  document.getElementById('password').value;
 if ((username=='ellabc23'&& password=='daspafounder')||(username=='lucas' && password=='daspafounder')){
-window.location.href='/daspa';
+window.location.href='/daspa/4qmni0ht34q0tyw890jmvqt39f0cnm9q3fnm0hviueno3hg0wqvnm5q2ctu8q490.html';
 }
 } 
